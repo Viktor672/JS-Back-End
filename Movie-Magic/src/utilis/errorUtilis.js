3130af17d5ae9d62122d89f1b3bbf97ad65bb625
Viktor <149080919+Viktor672@users.noreply.github.com>
2025-02-08 00:54:58 +0200

    Delete Movie-Magic directory

--- Movie-Magic/src/utilis/errorUtilis.js
+++
@@ -1,9 +0,0 @@
-export let getErrorMessage = (err) => {
-    if (err.name === 'ValidationError') {
-        return Object.values(err.errors)[0].message;
-        //err.errors.email.message
-    }
-    else {
-        return err.message;
-    }
-}
