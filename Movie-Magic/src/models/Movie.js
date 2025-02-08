290ef587cf9bb3b738e98651bc6284ed06464f71
Viktor <149080919+Viktor672@users.noreply.github.com>
2025-02-09 01:02:42 +0200

    Add files via upload

---
+++ Movie-Magic/src/models/Movie.js
@@ -0,0 +1,68 @@
+import { Schema, model, Types } from 'mongoose';
+
+let movieSchema = new Schema({
+    title: {
+        type: String,
+        minLength: [5, 'Should be at least 5 characters long!'],
+        maxLength: [250, 'Max length is 250 characters long!'],
+        required: [true, 'Title is required!'],
+        match: [/^[ a-zA-Z0-9]+$/, 'Title should be only English letters, digits, and whitespaces']
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: [
+            'tv-show',
+            'animation',
+            'movie',
+            'documentary',
+            'short-film',
+        ]
+    },
+    genre: {
+        type: String,
+        required: [true, 'Genre is required'],
+        minLength: [5, 'Genre should be at least 5 characters long!'],
+        maxLength: [250, 'Max length is 250 characters long!'],
+        match: [/^[ a-zA-Z0-9]+$/, 'Genre should be only English letters, digits, and whitespaces']
+    },
+    director: {
+        type: String,
+        minLength: [5, 'Director should be at least 5 characters long!'],
+        maxLength: [250, 'Max length is 250 characters long!'],
+
+    },
+    year: {
+        type: Number,
+        min: 1900,
+        max: 2025
+    },
+    imageUrl: {
+        type: String,
+        match: /^https?:\/\//
+    },
+    rating: {
+        type: Number,
+        default: 1,
+        min: 1,
+        max: 10
+    },
+    description: {
+        type: String,
+        minLength: 20,
+        match: [/^[ a-zA-Z0-9,.!?-]+$/, 'Description should be only English letters, digits, and whitespaces'],
+        trim:true
+    },
+    casts: [{
+        type: Types.ObjectId,
+        ref: 'Cast'
+    }],
+    creator: {
+        type: Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+let Movie = model('Movie', movieSchema);
+
+export default Movie;
